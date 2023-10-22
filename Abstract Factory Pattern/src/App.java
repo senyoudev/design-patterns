2ab2ac893c9e1644b82e4a06d59605f209e95a7e
senyoudev <younesmeskafe2018@gmail.com>
2023-10-22 15:55:13 +0100

    abstract factory

---
+++ Abstract Factory Pattern/src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Application app = configureApplication();
+        app.paint();
+    }
+
+      private static Application configureApplication() {
+        Application app;
+        GUIFactory factory;
+        String osName = System.getProperty("os.name").toLowerCase();
+        if (osName.contains("mac")) {
+            factory = new MacOSFactory();
+        } else {
+            factory = new WindowsFactory();
+        }
+        app = new Application(factory);
+        return app;
+    }
+}
